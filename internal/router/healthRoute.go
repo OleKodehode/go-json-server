e75bbea0774c72a3b41cff02640ab3296ee6d68d
Ole Kodehode <ole.kodehode@gmail.com>
2026-02-04 10:47:17 +0100

    Utility script + refactor of healthRoute

--- internal/router/healthRoute.go
+++ internal/router/healthRoute.go
@@ -1,13 +1,13 @@
 package router
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
+type HealthResponse struct {
+	Status string `json:"status"`
+}
+
 func HandleHealth(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	responseItem := map[string]string{"status": "ok"}
-	json.NewEncoder(w).Encode(responseItem)
+	RespondJSON(w, http.StatusOK, HealthResponse{Status: "ok"})
 }
---
+++ internal/router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Helper function
+func RespondJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+type ErrorMessage struct {
+	Error string `json:"error"`
+}
+
+func RespondError(w http.ResponseWriter, status int, message string) {
+	RespondJSON(w, status, ErrorMessage{Error:message})
+}
